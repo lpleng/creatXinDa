62db072c24e0cfe6d1f21edb47f573aa6f644fc4
hanwenqi <604450318@qq.com>
2017-05-22 09:59:39 +0800

    modify

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import Common from '@/views/Common'
 import Home from '@/views/Home'
 import Action from '@/views/Action'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <h3>这是首页全部产品</h3> 
+    <!---这是轮播部分-->
+    <div class="banner">
+        <div class="banner_right"><h3>这是轮播部分</h3></div>
+    </div>
+   <!-------------------这是轮播结束部分--------------->
+   <!--这是明星产品推荐部分-->
+<div class="star">
+    <div class="star_item">
+      明星产品推荐
+    </div>
+    <div class="star_body">
+
+    </div>
+</div>
+
+   <!------------<!这是明星产品推荐结束部分------------->-->
   </div>
-
 </template>
-
 <script>
 export default {
   name: 'hello',
@@ -15,8 +28,47 @@ export default {
   }
 }
 </script>
+<style scoped  lang="less">
+// ----------------------------这是公共样式部分
+ .mg{
+      margin :0 auto;
+  } 
+ .tex{
+    text-align: center;
+  }
 
 
-<style scoped>
+
+// ----------------------------这是公共样式结束部分
+// --------------------------这是轮播部分---------------
+
+  .banner{
+    width: 1200px;
+    height: 401px;
+    color: #fff;
+    .tex;
+    .mg;
+      .banner_right {
+       width: 1200px;
+       height: 401px;
+       background: #636363;
+     }
+  } 
+  // --------------------------这是轮播结束部分-----------------
+//  ------------------------- 这是明星产品推荐部分-------------------------
+ .star{
+  width: 1200px;
+  height: 473px;
+  margin-top: 50px;
+  background: pink;
+   .mg;
+ }
+
+
+
+
+
+// ------------------------这是明星产品推荐结束部分------------------------
+
 
 </style>
