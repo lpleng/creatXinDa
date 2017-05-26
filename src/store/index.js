b284a29e9269991e0f2970ca1dca835fbbfc55d0
李鹏 <627321463@qq.com>
2017-05-26 08:27:16 +0800

    lp update

--- src/store/index.js
+++ src/store/index.js
@@ -7,24 +7,34 @@ export default new Vuex.Store({
     //状态集合
     state:{
         //购物车数量
-        cartNum:0
+        cartNum:0,
+        shopKind:[]
     },
     //突变集合---用来操作状态集合
     mutations:{
         SETCARTNUM(state,num){
             state.cartNum = num;
+        },
+        SETKIND(state,num){
+            state.shopKind.push(mum);
         }
     },
     //动作集合---用来操作突变集合的
     actions:{
         setCartNum({commit},num){
             commit('SETCARTNUM',num);
+        },
+        setKind({cpmmit},num){
+            commit("SETKIND",num)
         }
     },
     //显示集合
     getters:{
         getCartNum(state){
             return state.cartNum
+        },
+        getKind(state){
+            return state.shopKind
         }
     }
 });
