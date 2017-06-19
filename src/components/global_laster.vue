ce9dcc5dd1e4c8b3c7cc9df2589988fe05ead245
李鹏 <627321463@qq.com>
2017-06-19 13:22:35 +0800

    #lp uodate

--- src/components/global_laster.vue
+++ src/components/global_laster.vue
@@ -86,24 +86,17 @@ export default {
 }
 </script>
 
-
 <style scoped lang="less">
-
-    .foot-posi{
-      position:fixed;
-      bottom:0;
-    }
-    .colposi{
-      background: rgba(255,255,255,.7);
-      width:25%;
-      height: 50px;
-      // margin-top:50px;  
-      float:left;
-      display:block;
-      cursor:pointer;  
-      // i{
-      //   font-weight: 700;
-      // }
-    
-  }
+.foot-posi{
+  position:fixed;
+  bottom:0;
+}
+.colposi{
+  background: rgba(255,255,255,.7);
+  width:25%;
+  height: 50px;
+  float:left;
+  display:block;
+  cursor:pointer;  
+}
 </style>
